647d050ff8256aa973938e0df2717b8a0aa8a9a6
Ansuman <sunmoonfortune@gmail.com>
2017-09-26 16:30:46 +0530

    Creating a server

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
